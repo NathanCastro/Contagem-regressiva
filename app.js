3eaea144596c75821c3272b35a1d4d4f3713a266
Nathan Castro <nathanslc18@gmail.com>
2022-02-10 09:17:49 -0300

    chore(exercicio):trying to play the date to page counter

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const minutesRow = document.querySelector("#minutes");
 const secondsRow = document.querySelector("#seconds");
 
 
-const endYear = new Date().getFullYear() + 1;
-const newYearTime = new Date( `January 01 ${endYear} 00:00:00 `);
+const endYear = new Date().getFullYear() +1 ;
+//const dateNow = document.querySelector('#dataCompleta');
+const newYearTime = new Date(`January 01 ${endYear} 00:00:00 `);
 
 const updateCountdown = () => {
     const currentTime = new Date();
@@ -15,11 +16,10 @@ const updateCountdown = () => {
     const minutes = Math.floor(difference / 1000 / 60 ) % 60;
     const seconds = Math.floor(difference / 1000) % 60;
     
-    secondsRow.textContent= seconds < 10 ? '0' + seconds : seconds;
-    minutesRow.textContent= minutes < 10 ? '0' + minutes : minutes;
-    hourRow.textContent= hours < 10 ? '0' + hours : hours;
-    daysRow.textContent= days < 10 ? '0' + days : days;
+    secondsRow.textContent = seconds < 10 ? '0' + seconds : seconds;
+    minutesRow.textContent = minutes < 10 ? '0' + minutes : minutes;
+    hourRow.textContent = hours < 10 ? '0' + hours : hours;
+    daysRow.textContent = days < 10 ? '0' + days : days;
 }
-
 
 setInterval(updateCountdown,1000)
