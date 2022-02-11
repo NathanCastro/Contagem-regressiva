3f430334080b65162b55f33fe7db76e34ea4fc34
Nathan Castro <nathanslc18@gmail.com>
2022-02-11 08:12:57 -0300

    chore(exercicio):ainda tentando e errando

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
+const valorInput = document.getElementById('dateInput').value;
+function verificar(){
+   
+    if(valorInput){
+        alert("Coloca a data correta")
+    }else{
+        var dateStorage = JSON.stringify(new Date(valorInput).value);
+        sessionStorage.setItem("dateInput", dateStorage);
+        window.location = "contador.html"
+    }
+}
+
+
 const daysRow = document.querySelector("#days");
 const hourRow = document.querySelector("#hours");
 const minutesRow = document.querySelector("#minutes");
 const secondsRow = document.querySelector("#seconds");
-
-
-const endYear = new Date().getFullYear() +1 ;
-//const dateNow = document.querySelector('#dataCompleta');
-const newYearTime = new Date(`January 01 ${endYear} 00:00:00 `);
+ 
 
 const updateCountdown = () => {
+    const newYearTime = new Date(JSON.parse(sessionStorage.getItem('dateInput')))
     const currentTime = new Date();
-    const difference = newYearTime - currentTime;
+    const difference = newYearTime.getTime() - currentTime.getTime();
+
     const days = Math.floor(difference / 1000 / 60 / 60 / 24);
     const hours = Math.floor(difference / 1000 / 60 / 60 ) % 24;
     const minutes = Math.floor(difference / 1000 / 60 ) % 60;
     const seconds = Math.floor(difference / 1000) % 60;
     
-    secondsRow.textContent = seconds < 10 ? '0' + seconds : seconds;
-    minutesRow.textContent = minutes < 10 ? '0' + minutes : minutes;
-    hourRow.textContent = hours < 10 ? '0' + hours : hours;
     daysRow.textContent = days < 10 ? '0' + days : days;
+    hourRow.textContent = hours < 10 ? '0' + hours : hours;
+    minutesRow.textContent = minutes < 10 ? '0' + minutes : minutes;
+    secondsRow.textContent = seconds < 10 ? '0' + seconds : seconds;
 }
 
 setInterval(updateCountdown,1000)
